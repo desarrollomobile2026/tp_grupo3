4ed7ee528950187ec9de56ccae4995a9231f2b65
Winona Muñoz <munozwinona8@gmail.com>
2026-06-24 01:33:55 -0300

    Modifico config.js

--- config.js
+++ config.js
@@ -1,9 +1,20 @@
 // config.js - Archivo de configuración local (Excluido de Git por seguridad)
+// const firebaseConfig = {
+//    apiKey: "AIzaSyBR469f8QnBVG9p6TXVvNMYjfxk2SxPlm4",
+//    authDomain: "app-clase-sebastian.firebaseapp.com",
+//    projectId: "app-clase-sebastian",
+//    storageBucket: "app-clase-sebastian.firebasestorage.app",
+//    messagingSenderId: "495895860460",
+//    appId: "1:495895860460:web:712a30fa4ab544aa2841c9"
+//};
+
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBR469f8QnBVG9p6TXVvNMYjfxk2SxPlm4",
-    authDomain: "app-clase-sebastian.firebaseapp.com",
-    projectId: "app-clase-sebastian",
-    storageBucket: "app-clase-sebastian.firebasestorage.app",
-    messagingSenderId: "495895860460",
-    appId: "1:495895860460:web:712a30fa4ab544aa2841c9"
+  apiKey: "AIzaSyDgIr5llxca7H5l1aS6L1Bkerycc28mJT0",
+  authDomain: "reina-stock.firebaseapp.com",
+  projectId: "reina-stock",
+  storageBucket: "reina-stock.firebasestorage.app",
+  messagingSenderId: "947730598715",
+  appId: "1:947730598715:web:6fe5e2725d7b42f1d9c51e",
+  measurementId: "G-R1LD2JS4NE"
 };
